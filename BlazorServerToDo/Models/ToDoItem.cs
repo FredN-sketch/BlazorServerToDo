a0c6c180ea7f0c1bf25d546be55ce327b0995fe1
Fredrik Norgren <fredrik.norgren@gmail.com>
2025-08-06 16:18:44 +0200

    Rename class and refactor

---
+++ BlazorServerToDo/Models/ToDoItem.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerToDo.Models
+{
+    public class ToDoItem // : IValidatableObject 
+    {
+        [Required(ErrorMessage = "Title is required.")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Description is required.")]
+        public string Description { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            //do something here if needed
+            if (string.IsNullOrWhiteSpace(Title))
+            {
+                yield return new ValidationResult("Title cannot be empty.", new[] { nameof(Title) });
+            }
+            if (string.IsNullOrWhiteSpace(Description))
+            {
+                yield return new ValidationResult("Description cannot be empty.", new[] { nameof(Description) });
+            }
+        }
+    }
+}
